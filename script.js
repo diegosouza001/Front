85ccacbcce03d2f7c4404e78bd4241955fb3d6be
Diego Souza <diegoazevedo0807@gmail.com>
2024-12-06 23:46:16 -0300

    Mudança no JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('clickMe').addEventListener('click', () => {
     const message = document.getElementById('message');
-    message.textContent = 'You clicked the button!';
+    message.textContent = 'Você clicou no botão!';
     message.style.color = '#6200ea';
   });
